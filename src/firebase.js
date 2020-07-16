137f0470c07b30d6e7a02f3c807f021c985510a6
myself <android.programmation@laposte.net>
2020-07-16 15:50:17 +0200

    Remove firebase storage package

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import "firebase/storage";
 import { firebaseConfig } from "./config";
 
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-export const storage = firebase.storage();
